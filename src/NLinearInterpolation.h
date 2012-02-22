1d7a23d25395113f5e2700fdad85ee232a86ba34
Pablo Strasser <strassp6@etu.unige.ch>
2012-02-22 13:49:28 +0100

    Rewritter NLinear Interpolation with template meta prog.

--- src/NLinearInterpolation.h
+++ src/NLinearInterpolation.h
@@ -7,6 +7,42 @@
 
 #include <iostream>
 
+template <int type_dim,class type_data,int prof,int type_stop,class TypeWorld>
+struct SubElement
+{
+	static type_data Do( Physvector<type_dim,int>& el,const Physvector<type_dim,type_data>& posdelta0scaled,const Physvector<type_dim,int> &key0 ,int ind,TypeWorld &m_world)
+	{
+		SubElement<type_dim,type_data,prof+1,type_stop-1,TypeWorld> SE;
+		el.Set(prof,0);
+		type_data a=SE.Do(el,posdelta0scaled,key0,ind,m_world);
+		el.Set(prof,1);
+		type_data b=SE.Do(el,posdelta0scaled,key0,ind,m_world);
+		return a+b;
+	}
+};
+
+template <int type_dim,class type_data,int prof,class TypeWorld>
+struct SubElement<type_dim,type_data,prof,1,TypeWorld>
+{
+	static type_data Do( Physvector<type_dim,int>& el,const Physvector<type_dim,double>& posdelta0scaled,const Physvector<type_dim,int> &key0 ,int ind,TypeWorld &m_world)
+	{
+		type_data ret=1;
+		for(int i=1;i<=type_dim;++i)
+		{
+			ret*=(posdelta0scaled.Get(i)-1+el.Get(i) )*(-1+2*el.Get(i));
+		}
+		Physvector<type_dim,type_data> vtemp;
+		if(m_world.m_mac_grid.Exist(key0+el))
+		{
+		m_world.m_mac_grid[key0+el].GetSpeed(vtemp);
+		ret*=vtemp.Get(ind);
+		return ret;
+		}
+		else{
+			return 0;
+		}
+	}
+};
 /**
  * @file NLinearInterpolation.h
  * @brief
@@ -41,7 +77,6 @@ class NLinearInterpolation
 	 * \exception InternalError if prof is bigger than the stop condition.
 	 * This cannot normaly happen.
 	 **/
-	inline type_data SubElement( Physvector<type_dim,int>& el,const Physvector<type_dim,double>& posdelta0scaled,const Physvector<type_dim,int> &key0 ,int ind,int prof=1);
 public:
 
 /**
@@ -59,9 +94,6 @@ NLinearInterpolation(TypeWorld &world,const Physvector<type_dim,type_data> &_1_h
  * @return type_data Result for the given speed.
  **/
 type_data	Calculate(Physvector<type_dim,type_data> pos,int ind);
- #ifdef TesThrowIntExcept
- void Boum();
- #endif
 };
 #include "NLinearInterpolation.tpp"
 #endif
--- src/NLinearInterpolation.tpp
+++ src/NLinearInterpolation.tpp
@@ -22,47 +22,6 @@ typename TypeWorld::type_data	NLinearInterpolation<TypeWorld>::Calculate( Physve
 		pos.GetRef(i)-=key0.GetRef(i);
 	}
 	Physvector<type_dim,int> el;
-	return SubElement(el,pos,key0,ind);
+	SubElement<type_dim,type_data,1,type_dim+1,TypeWorld> SE;
+	return SE.Do(el,pos,key0,ind,m_world);
 }
-
-template <class TypeWorld>
-typename TypeWorld::type_data NLinearInterpolation<TypeWorld>::SubElement(Physvector<type_dim,int>& el,const Physvector<type_dim,double>& posdelta0scaled,const Physvector<type_dim,int> &key0 ,int ind,int prof)
-{
-	if(prof<=type_dim&&prof>=0){
-	el.Set(prof,0);
-	type_data a=SubElement(el,posdelta0scaled,key0,ind,prof+1);
-	el.Set(prof,1);
-	type_data b=SubElement(el,posdelta0scaled,key0,ind,prof+1);
-	return a+b;
-	}
-	else if(prof==type_dim+1)
-	{
-		type_data ret=1;
-		for(int i=1;i<=type_dim;++i)
-		{
-			ret*=(posdelta0scaled.Get(i)-1+el.Get(i) )*(-1+2*el.Get(i));
-		}
-		Physvector<type_dim,type_data> vtemp;
-		if(m_world.m_mac_grid.Exist(key0+el))
-		{
-		m_world.m_mac_grid[key0+el].GetSpeed(vtemp);
-		ret*=vtemp.Get(ind);
-		return ret;
-		}
-		else{
-			return 0;
-		}
-	}
-	throw InternalError("Normally we don't get there!!!!");
-}
-
-
- #ifdef TesThrowIntExcept
- template <class TypeWorld>
- void NLinearInterpolation<TypeWorld>::Boum()
- {
-	 Physvector<type_dim,int> el;
-	 const Physvector<type_dim,double> posdelta0scaled;
-	 const Physvector<type_dim,int> key0;
-	 SubElement(el,posdelta0scaled,key0,3,type_dim+10);}
- #endif
