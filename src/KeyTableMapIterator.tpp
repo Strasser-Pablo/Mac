2cec7dd17cfe598ee5c79a93a79b89cb4c250076
Pablo Strasser <strassp6@etu.unige.ch>
2012-03-20 23:29:35 +0100

    Added a table that is a hash table.

--- src/KeyTableMapIterator.tpp
+++ src/KeyTableMapIterator.tpp
@@ -1,4 +1,3 @@
-
 /**
  * @file KeyTableMapIterator.tpp
  * @brief
--- src/KeyTableUnorderedMap.h
+++ src/KeyTableUnorderedMap.h
@@ -1,4 +1,4 @@
-#include "KeyTableUnorderedMap.h"
+#include "KeyTable.h"
 #ifndef KeyTableUnordered_H
 #define KeyTableUnordered_H
 #include <unordered_map>
@@ -19,12 +19,12 @@ using namespace std;
  * \tparam TypeComp Class overloading () operator giving if the first key is before the second.
  **/
 template<class TypeKey,class TypeData,class TypeHash=std::hash<TypeKey>,class TypeComp=equal_to<TypeKey> >
-class KeyTableUnorderedMap:public KeyTable<TypeKey,TypeData,KeyTableUnorderedMapIterator<TypeKey,TypeData,TypeComp>,KeyTableUnorderedMapConstIterator<TypeKey,TypeData,TypeComp>,typename map<TypeKey,TypeData>::size_type>{
+class KeyTableUnorderedMap:public KeyTable<TypeKey,TypeData,KeyTableUnorderedMapIterator<TypeKey,TypeData,TypeHash,TypeComp>,KeyTableUnorderedMapConstIterator<TypeKey,TypeData,TypeHash,TypeComp>,typename map<TypeKey,TypeData>::size_type>{
 	unordered_map<TypeKey,TypeData,TypeHash,TypeComp> m_map;
 public:
-	typedef typename map<TypeKey,TypeData,TypeHash,TypeComp>::size_type size_type;
-	typedef KeyTableUnorderedMapIterator<TypeKey,TypeData,TypeComp> iterator;
-	typedef KeyTableUnorderedMapConstIterator<TypeKey,TypeData,TypeComp>  const_iterator;
+	typedef typename unordered_map<TypeKey,TypeData,TypeHash,TypeComp>::size_type size_type;
+	typedef KeyTableUnorderedMapIterator<TypeKey,TypeData,TypeHash,TypeComp> iterator;
+	typedef KeyTableUnorderedMapConstIterator<TypeKey,TypeData,TypeHash,TypeComp>  const_iterator;
 	typedef TypeData type_data;
 	typedef TypeKey type_key;
 	/**
@@ -32,7 +32,7 @@ public:
 	 * Construct an empty map with ordening given by argument.
 	 * @param comp Class overloading () that compare two key and return true if first is before than second.
 	 **/
-	KeyTableUnorderedMap(const TypeComp& comp=less<TypeKey>() );
+	KeyTableUnorderedMap(const TypeHash& hash=std::hash<TypeKey>(),const TypeComp& comp=equal_to<TypeKey>());
 	virtual ~KeyTableUnorderedMap();
 	virtual void insert(const TypeKey & key,const TypeData & value);
 	virtual bool empty();
@@ -41,7 +41,7 @@ public:
 	virtual TypeData& operator[](const TypeKey & key);
 	virtual iterator begin();
 	virtual iterator end();
-	virtual const_iterator begin()const;
+	virtual const_iterator begin() const;
 	virtual const_iterator end() const;
 	virtual void erase(iterator pos);
 	virtual void erase(const TypeKey & key);
