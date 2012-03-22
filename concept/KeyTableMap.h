ba77e0fece5c0b1c8f98fd0f2a61700e2df3e881
Pablo Strasser <strassp6@etu.unige.ch>
2012-03-22 18:30:56 +0100

    KeyTableMap Concept.

--- concept/KeyTableMap.h
+++ concept/KeyTableMap.h
@@ -1,28 +1,25 @@
 /**
- * @brief KeyTableMap
+ * @brief Container mapping a key to a object.
  *
+ * In summary the following method are needed:
+ * - Iterator and constant iterator.
+ * - Insert operator and get operator.
+ * - Serialization operator.
  *
- *
+ * Note that the constructor is not part of the concept.
  **/
-template<class TypeKey,class TypeData,class TypeComp=less<TypeKey> >
+template<class TypeKey,class TypeData>
 class KeyTableMap
 {
 	friend class boost::serialization::access;
 	template <class Archive>
 	void serialize(Archive & Ar,const unsigned int version);
 public:
-	typedef typename map<TypeKey,TypeData,TypeComp>::size_type size_type;
-	typedef KeyTableMapIterator<TypeKey,TypeData,TypeComp> iterator;
-	typedef KeyTableMapConstIterator<TypeKey,TypeData,TypeComp>  const_iterator;
+	typedef size_t size_type;
+	typedef KeyTableMapIterator<TypeKey,TypeData> iterator;
+	typedef KeyTableMapConstIterator<TypeKey,TypeData>  const_iterator;
 	typedef TypeData type_data;
 	typedef TypeKey type_key;
-	/**
-	 * @brief
-	 * Construct an empty map with ordening given by argument.
-	 * @param comp Class overloading () that compare two key and return true if first is before than second.
-	 **/
-	KeyTableMap(const TypeComp& comp=less<TypeKey>() );
-	virtual ~KeyTableMap();
 	virtual void insert(const TypeKey & key,const TypeData & value);
 	virtual bool empty() __attribute__((pure));
 	virtual size_type size() __attribute__((pure));
@@ -35,4 +32,5 @@ public:
 	virtual void erase(iterator pos);
 	virtual void erase(const TypeKey & key);
 	virtual void clear();
+	virtual void insert(size_type count);
 };
