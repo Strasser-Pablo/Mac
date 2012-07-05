89522915e8f5c292cf94f58f813336aa6885898b
Pablo Strasser <strassp6@etu.unige.ch>
2012-07-05 21:31:56 +0200

    Added Policy to calculate Gradiant

---
+++ src/Policy_Gradiant.h
@@ -0,0 +1,36 @@
+#ifndef Policy_Gradiant_H
+#define Policy_Gradiant_H
+
+template<typename Data>
+class Policy_Gradiant
+{
+	typedef typename Data::type_data_struct::type_Data_Grid type_Data_Grid;
+	typedef typename type_Data_Grid::type_data_mac_cell type_data_mac_cell;
+	typedef typename type_data_mac_cell::type_pressure type_pressure;
+	typedef typename type_Data_Grid::type_data_key type_data_key;
+	typedef typename type_Data_Grid::type_data_key_value type_data_key_value;
+	typedef typename type_Data_Grid::type_data_vector type_data_vector;
+	typedef typename type_Data_Grid::type_data_value type_data_value;
+	typedef typename type_Data_Grid::type_data_neigh type_data_neigh;
+	typedef typename type_data_mac_cell::type_speed type_speed;
+	static const int type_dim=type_Data_Grid::type_dim;
+	const type_data_vector &m_1_h;
+	type_Data_Grid& m_grid;
+	public:
+	Policy_Gradiant(Data& data) :m_1_h(data.m_data.GetGridData().m_h.GetRef_Inv()),m_grid(data.m_data.GetGridData())
+	{
+	}
+	type_speed Get_Gradiant(type_data_neigh* m_neigh)
+	{
+		type_speed ret;
+		for(int i=1;i<=type_dim;++i)
+		{
+			type_data_value temp=m_neigh->GetRef().Pressure_Get().Get()*m_1_h.Get(i);
+			temp-=m_neigh->GetNeighbour(i,-1)->GetRef().Pressure_Get().Get()*m_1_h.Get(i);
+			ret.GetRef(i)=temp;
+		}
+		return ret;
+	}
+};
+
+#endif
---
+++ test/TestPolicy_Gradiant.h
@@ -0,0 +1,183 @@
+#include <cxxtest/TestSuite.h>
+#include "../src/Neighbour_List.h"
+#include "../src/Hookable_Hash_Table.h"
+#include "../src/Hook_Neighbour_List.h"
+#include "../src/Physvector.h"
+#include "../src/HashPhysvector.h"
+#include "../src/Data_CellType_Fluid_Air.h"
+#include "../src/Data_Base_Dim_Type.h"
+#include "../src/Data_Grid_MacCell.h"
+#include "../src/Data_Grid_Speed.h"
+#include "../src/Data_Grid_Pressure.h"
+#include "../src/Datas.h"
+#include "../src/Data_Staggered_Left.h"
+#include "../src/Data_Grid_Layer.h"
+#include "../src/DataRef.h"
+#include "../src/Policies.h"
+#include "../src/Data_Grid_Data.h"
+#include "../src/Algorithms.h"
+#include "../src/Algorithms_Delete_MacCell.h"
+#include "../src/Data_Grid_Table.h"
+#include "../src/Empty.h"
+#include "../src/Data_Topology.h"
+#include "../src/Data_Particle.h"
+#include "../src/Data_Particles_List.h"
+#include "../src/Data_Grid_Base_Spacing.h"
+#include "../src/Data_Grid_Layer_Empty.h"
+#include "../src/Data_CellType_Const_Inbound.h"
+#include "../src/Policy_Gradiant.h"
+#define eps 1e-10
+class TestPolicy_Gradiant : public CxxTest::TestSuite  //LCOV_EXCL_LINE 
+{
+	public:
+	void test1d1()
+	{
+		typedef Data_Base_Dim_Type<double,1> DataBase0;
+		DataBase0 base0;
+		typedef Data_Staggered_Left<DataBase0> DataBase;
+		DataBase base(base0);
+		typedef typename DataBase::type_data_value type_data_value;
+		typedef Data_CellType_Fluid_Air<DataBase> type_cell_type;
+		typedef Data_Grid_Speed<DataBase> type_grid_speed;
+		typedef Data_Grid_Pressure<DataBase> type_grid_pressure;
+		type_grid_speed m_grid_speed;
+		typedef Data_CellType_Const_Inbound<type_grid_speed> type_grid_speed2;
+		type_grid_speed2 m_grid_speed2(m_grid_speed);
+		type_grid_pressure m_grid_pressure;
+		Data_CellType_Fluid_Air<DataBase> D(base);
+		Inversible_Value<type_data_value> rho_air(1);
+		Inversible_Value<type_data_value> rho_fluid(1000);
+		D.SetRhoAir(rho_air);
+		D.SetRhoFluid(rho_fluid);
+		typedef Data_Grid_Layer<int> type_layer;
+		type_layer layer;
+		typedef Data_Grid_Layer_Empty<type_layer> type_layer2;
+		type_layer2 layer2;
+		typedef Datas<DataBase,type_cell_type,type_grid_speed2,type_grid_pressure,type_layer2> type_datas;
+		type_datas m_datas(base,D,m_grid_speed2,m_grid_pressure,layer2);
+		typedef Data_Grid_MacCell<type_datas> type_mac_cell;
+		type_mac_cell mac(m_datas);
+		typedef Physvector<1,int> vect;
+		typedef HashPhysvector<1,int> Hash;
+		Hash hash;
+		typedef Hookable_Hash_Table<Hook_Neighbour_List,vect,Neighbour_List<1,type_mac_cell>,true,Hash> hook_table;
+		Neighbour_List<1,type_mac_cell> m_neigh(mac);
+		hook_table m_hook_table(m_neigh,hash);
+		typedef Data_Grid_Table<hook_table> type_data_grid;
+		type_data_grid table(m_hook_table);
+		typedef Data_Grid_Base_Spacing<type_data_grid> DataBase_00;
+		DataBase_00 base_00(table);
+		Physvector<1,double> h;
+		h.Set(1,1.0);
+		base_00.m_h.Set(h);
+		typedef Data_Grid_Data<DataBase_00,DataBase> type_grid_data;
+		type_grid_data m_grid_data(base_00,base);
+
+		typedef Data_Particle<DataBase> type_particle;
+		typedef Data_Particles_List<type_particle,DataBase> type_particles;
+		type_particles particles(base);
+		typedef Data_Topology<type_particles,type_grid_data> type_topology;
+		type_topology topo(particles,m_grid_data);
+		typedef DataRef<type_topology> type_data_ref;
+		type_data_ref m_data_ref(topo);
+		
+		vect v;
+		v.Set(1,0);
+		Data_Pressure_Data<double> pressure;
+		pressure.Set(1.0);
+		m_data_ref.m_data.GetGridData()[v].GetRef().Pressure_Set(pressure);
+		
+		v.Set(1,-1);
+		pressure.Set(0);
+		m_data_ref.m_data.GetGridData()[v].GetRef().Pressure_Set(pressure);
+
+		Policy_Gradiant<type_data_ref> m_pol(m_data_ref);
+
+		v.Set(1,0);
+		
+		TS_ASSERT_DELTA(m_pol.Get_Gradiant(&m_data_ref.m_data.GetGridData()[v]).Get(1),1.0,eps);
+
+		v.Set(1,-1);
+		pressure.Set(1.0);
+		m_data_ref.m_data.GetGridData()[v].GetRef().Pressure_Set(pressure);
+
+		v.Set(1,0);
+
+		TS_ASSERT_DELTA(m_pol.Get_Gradiant(&m_data_ref.m_data.GetGridData()[v]).Get(1),0.0,eps);
+	}
+	void test2d1()
+	{
+		typedef Data_Base_Dim_Type<double,2> DataBase0;
+		DataBase0 base0;
+		typedef Data_Staggered_Left<DataBase0> DataBase;
+		DataBase base(base0);
+		typedef typename DataBase::type_data_value type_data_value;
+		typedef Data_CellType_Fluid_Air<DataBase> type_cell_type;
+		typedef Data_Grid_Speed<DataBase> type_grid_speed;
+		typedef Data_Grid_Pressure<DataBase> type_grid_pressure;
+		type_grid_speed m_grid_speed;
+		typedef Data_CellType_Const_Inbound<type_grid_speed> type_grid_speed2;
+		type_grid_speed2 m_grid_speed2(m_grid_speed);
+		type_grid_pressure m_grid_pressure;
+		Data_CellType_Fluid_Air<DataBase> D(base);
+		Inversible_Value<type_data_value> rho_air(1);
+		Inversible_Value<type_data_value> rho_fluid(1000);
+		D.SetRhoAir(rho_air);
+		D.SetRhoFluid(rho_fluid);
+		typedef Data_Grid_Layer<int> type_layer;
+		type_layer layer;
+		typedef Data_Grid_Layer_Empty<type_layer> type_layer2;
+		type_layer2 layer2;
+		typedef Datas<DataBase,type_cell_type,type_grid_speed2,type_grid_pressure,type_layer2> type_datas;
+		type_datas m_datas(base,D,m_grid_speed2,m_grid_pressure,layer2);
+		typedef Data_Grid_MacCell<type_datas> type_mac_cell;
+		type_mac_cell mac(m_datas);
+		typedef Physvector<2,int> vect;
+		typedef HashPhysvector<2,int> Hash;
+		Hash hash;
+		typedef Hookable_Hash_Table<Hook_Neighbour_List,vect,Neighbour_List<2,type_mac_cell>,true,Hash> hook_table;
+		Neighbour_List<2,type_mac_cell> m_neigh(mac);
+		hook_table m_hook_table(m_neigh,hash);
+		typedef Data_Grid_Table<hook_table> type_data_grid;
+		type_data_grid table(m_hook_table);
+		typedef Data_Grid_Base_Spacing<type_data_grid> DataBase_00;
+		DataBase_00 base_00(table);
+		Physvector<2,double> h;
+		h.Set(1,1.0);
+		h.Set(2,1.0);
+		base_00.m_h.Set(h);
+		typedef Data_Grid_Data<DataBase_00,DataBase> type_grid_data;
+		type_grid_data m_grid_data(base_00,base);
+
+		typedef Data_Particle<DataBase> type_particle;
+		typedef Data_Particles_List<type_particle,DataBase> type_particles;
+		type_particles particles(base);
+		typedef Data_Topology<type_particles,type_grid_data> type_topology;
+		type_topology topo(particles,m_grid_data);
+		typedef DataRef<type_topology> type_data_ref;
+		type_data_ref m_data_ref(topo);
+		
+		vect v;
+		v.Set(1,0);
+		v.Set(2,0);
+		Data_Pressure_Data<double> pressure;
+		pressure.Set(1.0);
+		m_data_ref.m_data.GetGridData()[v].GetRef().Pressure_Set(pressure);
+		
+		v.Set(1,-1);
+		v.Set(2,0);
+		pressure.Set(0);
+		m_data_ref.m_data.GetGridData()[v].GetRef().Pressure_Set(pressure);
+
+		v.Set(1,0);
+		v.Set(2,-1);
+		pressure.Set(0);
+		m_data_ref.m_data.GetGridData()[v].GetRef().Pressure_Set(pressure);
+
+		v.Set(1,0);
+		v.Set(2,0);
+
+		Policy_Gradiant<type_data_ref> m_pol(m_data_ref);
+		TS_ASSERT_DELTA(m_pol.Get_Gradiant(&m_data_ref.m_data.GetGridData()[v]).Get(1),1.0,eps);
+	}
+};
