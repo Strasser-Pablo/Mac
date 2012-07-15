2768a85288edce7c51ff101c77f587e25c23b339
Pablo Strasser <strassp6@etu.unige.ch>
2012-07-15 23:55:18 +0200

    Added Algorithm inflow and Solid to Const

---
+++ src/Algorithms_Inflow_To_Const.h
@@ -0,0 +1,36 @@
+#ifndef Algorithms_Inflow_To_Const_H
+#define Algorithms_Inflow_To_Const_H
+
+template <typename DataType,typename Policy>
+class Algorithms_Inflow_To_Const : public Policy
+{
+	typedef typename DataType::type_data_struct type_data;
+	typedef typename type_data::type_Data_Grid type_grid;
+	typedef typename type_grid::type_data_mac_cell type_data_grid;
+	typedef typename type_data_grid::type_data_value type_data_value;
+	typedef typename type_data_grid::type_data_vector type_data_vector;
+	typedef typename type_grid::iterator iterator;
+	typedef typename type_data::type_Data_Timing type_Data_Timing;
+	typedef typename type_Data_Timing::type_Time_Type type_Time_Type;
+	static const int type_dim=type_grid::type_dim;
+	type_grid& m_grid;
+	public:
+	Algorithms_Inflow_To_Const(DataType data,const Policy& pol) : Policy(pol),m_grid(data.m_data.GetGridData())
+	{
+	}
+	void Do()
+	{
+		for(iterator it=m_grid.begin();it!=m_grid.end();++it)
+		{
+			if(it.data().GetRef().GetIsInflow())
+			{
+				for(int i=1;i<=type_dim;++i)
+				{
+					it.data().GetRef().Speed_Set_Const(i);
+				}
+			}
+		}
+	}
+};
+
+#endif
