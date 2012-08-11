9284a9aa1c5079599ba75ed2b2f427a08424bc88
Pablo Strasser <strassp6@etu.unige.ch>
2012-08-12 00:13:23 +0200

    Changed Algorithm_Initialize.

--- src/Algorithms_Initialize_MacCell.h
+++ src/Algorithms_Initialize_MacCell.h
@@ -7,17 +7,16 @@ class Algorithms_Initialize_MacCell : public Policy
 	typedef typename DataType::type_data_struct type_data;
 	typedef typename type_data::type_Data_Grid type_grid;
 	typedef typename type_grid::iterator iterator;
-	type_data& m_data;
 	type_grid& m_grid;
 	public:
-	Algorithms_Initialize_MacCell(DataType data,const Policy& pol) :Policy(pol),m_data(data.m_data), m_grid(m_data.GetGridData()) 
+	Algorithms_Initialize_MacCell(DataType data,const Policy& pol) :Policy(pol), m_grid(data.m_data.GetGridData()) 
 	{
 	}
 	void Do()
 	{
 		for(iterator it=m_grid.begin();it!=m_grid.end();++it)
 		{
-			it.data().GetRef().SetLayerEmpty();
+			it.data().Layer_GetRef().SetLayerEmpty();
 		}
 	}
 };
