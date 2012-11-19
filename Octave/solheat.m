b75caa9b63a0f38759669f1b3332cfb8b1640d7a
Pablo Strasser <strassp6@etu.unige.ch>
2012-11-19 20:59:57 +0100

    Corrected Octave script

--- Octave/solheat.m
+++ Octave/solheat.m
@@ -2,7 +2,7 @@ function [u] = solheat(x,nmax,t,u0,L,nu)
 	u=u0*(1.-x/L);
 	for n=1:nmax
 		if mod(n,2)==0
-			u=u+4*u0/(n*pi)*(-(-1)^(n/2))*sin(n*pi*x/(2*L))*exp(-n^2*pi^2*nu*t/(4*L));
+			u=u+4*u0/(n*pi)*(-(-1)^(n/2))*sin(n*pi*x/(2*L))*exp(-n^2*pi^2*nu*t/(4*L^2));
 		end
 	end
 end
